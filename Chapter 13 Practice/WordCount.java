9772243a5051c9f67a07a0637c9f72c3b948747e
Kalevyx <CRLi@student.naperville203.org>
2016-03-21 15:09:28 -0500

    March 21, 2016

---
+++ Chapter 13 Practice/WordCount.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+
+public class WordCount{
+    public static void main(String[]args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is the file name? ");}}
