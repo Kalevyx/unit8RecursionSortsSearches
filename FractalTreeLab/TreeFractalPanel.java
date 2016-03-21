10b001c9b88a4fcb693522ed0a3fa6f2611f5ac4
Kalevyx <kalevyx@gmail.com>
2016-03-20 22:55:57 -0500

    Update TreeFractalPanel.java

--- FractalTreeLab/TreeFractalPanel.java
+++ FractalTreeLab/TreeFractalPanel.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.awt.geom.Line2D;
 import javax.swing.JPanel;
 
 public class TreeFractalPanel extends JPanel{
@@ -8,26 +9,51 @@ public class TreeFractalPanel extends JPanel{
     private final int BOTTOM_X = 250, BOTTOM_Y = 450;
     private final int TOP_X = 250, TOP_Y = 350;
     
-    private double fractionLength;
+    private final double ANGLE_RELATIVE_VERTICAL = 90;
+    private double angle = 15;
+    
+    private double fractionLength = .75;
     private double smallestBranch;
-    private double angle;
-    private double angle1;
-    private double angle2;
+    private double length = 100;
+    private double angle1 = 0;
+    private double angle2 = 0;
+    private final double THETA = 15;
     private int endX1, endY1;
     private int endX2, endY2;
     
+    private double startX = 250;
+    private double startY = 350;
     
-    public void drawFractal(Graphics2D g2, double startX, double startY, double length, double angleRelativeVertical){
-        angle1 += angleRelativeVertical;
-        angle2 -= angleRelativeVertical;
-        length *= fractionLength;
-        int endX1 = (int) (startX-length*Math.sin(angle1));
-        int endy1 = (int) (startY-length*Math.cos(angle1));
-        int endX2 = (int) (startX-length*Math.sin(angle2));
-        int endY2 = (int) (startY-length.Math.cos(angle2));
-        g2.draw(new Line2D.Double(BOTTOM_X, BOTTOM_Y, TOP_X, TOP_Y));}
+    private int current;
+    
+    public TreeFractalPanel(int currentOrder){
+        current = currentOrder;
+        setBackground(Color.black);
+        setPreferredSize(new Dimension(PANEL_WIDTH,PANEL_HEIGHT));}
+    
+    public void drawFractal(int order, Graphics2D g2, double startX, double startY, double length, double angle){
+        if(order == 1){
+            return;}
+        else{
+            //angle1 += angle;
+            //angle2 -= angle;
+            length *= fractionLength;
+            int endX1 = (int) (startX+length*Math.sin(Math.toRadians(angle)));
+            int endY1 = (int) (startY-length*Math.cos(Math.toRadians(angle)));
+            //int endX2 = (int) (startX-length*Math.sin(Math.toRadians(angle2)));
+            //int endY2 = (int) (startY-length*Math.cos(Math.toRadians(angle2)));
+            g2.draw(new Line2D.Double(startX, startY, endX1, endY1));
+            g2.draw(new Line2D.Double(startX, startY, endX1, endY1));
+            drawFractal(order-1, g2, endX1, endY1, length, angle+THETA);
+            drawFractal(order-1, g2, endX1, endY1, length, angle-THETA);}}
+    
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.blue);
+        drawFractal(current, g2, startX, startY, length, Math.toRadians(ANGLE_RELATIVE_VERTICAL));}
+        //g2.draw(new Line2D.Double(100, endX1, endY1, 0));}
         
-    public void paintComponent(Graphics2D g2){
-        super.paintComponent(g2);
-        
-        drawFractal(g2, BOTTOM_X, BOTTOM_Y, TOP_X, TOP_Y,Math.toRadian());}}
+    public void setOrder(int order){current = order;}
+    
+    public int getOrder(){return current;}}
