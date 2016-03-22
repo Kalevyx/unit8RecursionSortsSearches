a93d1a086e19f892e90d5dd8222a8778df4e5ecb
Kalevyx <CRLi@student.naperville203.org>
2016-03-22 15:05:35 -0500

    March 22, 2016

--- Chapter 13 Practice/WordCount.java
+++ Chapter 13 Practice/WordCount.java
@@ -1,6 +1,24 @@
 import java.util.Scanner;
+import java.io.File;
 
 public class WordCount{
-    public static void main(String[]args){
+    public static void main(String[]args)
+    {
         Scanner scan = new Scanner(System.in);
-        System.out.println("What is the file name? ");}}
+        System.out.println("What is the file name? ");
+        String filename = scan.next();
+        
+        File inputFile = new File(filename);
+        Scanner in = new Scanner(inputFile);
+        
+        int countChars = 0;
+        int countWords = 0;
+        int countLines = 0;
+        
+        in.useDelimiter("");
+        while(in.hasNext()){
+            countChars++;}
+        in.close();    
+        System.out.println("Number of Characters: "+countChars);
+    }
+}
