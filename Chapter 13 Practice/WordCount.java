8a9562035c6ef58306a041681cdae4ab23d1d23c
Kalevyx <CRLi@student.naperville203.org>
2016-03-23 15:08:53 -0500

    March 23, 2016

--- Chapter 13 Practice/WordCount.java
+++ Chapter 13 Practice/WordCount.java
@@ -1,24 +1,32 @@
 import java.util.Scanner;
 import java.io.File;
+import java.io.FileNotFoundException;
 
 public class WordCount{
     public static void main(String[]args)
     {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("What is the file name? ");
-        String filename = scan.next();
         
-        File inputFile = new File(filename);
-        Scanner in = new Scanner(inputFile);
-        
-        int countChars = 0;
-        int countWords = 0;
-        int countLines = 0;
-        
-        in.useDelimiter("");
-        while(in.hasNext()){
-            countChars++;}
-        in.close();    
-        System.out.println("Number of Characters: "+countChars);
+        try{
+            Scanner scan = new Scanner(System.in);
+            System.out.print("What is the file name? ");
+            String filename = scan.next();
+            
+            File inputFile = new File(filename);
+            Scanner in = new Scanner(inputFile);
+            
+            in.useDelimiter("");
+            
+            int countChars = 0;
+            int countWords = 0;
+            int countLines = 0;
+            while(in.hasNext()){
+                in.next();
+                countWords++;}
+            in.close();    
+            System.out.println("Number of Words: "+countWords);
+            while(}
+            
+        catch(FileNotFoundException exception){
+            System.out.println("File not found");}
     }
 }
