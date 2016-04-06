aa90237a3629b0efb6967f4ee483e2c0c1dbf4a1
Kalevyx <CRLi@student.naperville203.org>
2016-04-06 15:07:08 -0500

    April 6, 2016

---
+++ MergeSort/BubbleSort.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class BubbleSort{
+    public static int[] swap(int[] a){
+        for(int i = 1; i < a.length-1; i++){
+            if(a[i] < a[i-1]){
+                int temp = a[i];
+                a[i] = a[i-1];
+                a[i-1] = temp;}}
+                
+        return a;}
+                
+    public static void main(String[] args){
+        int[] a = ArrayUtil.randomIntArray(20, 100);
+        System.out.println(Arrays.toString(a));
+        
+        BubbleSort.swap(a);
+        System.out.println(Arrays.toString(a));}}
